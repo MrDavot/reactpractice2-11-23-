f17ee0928c8ca49997798c11ab4250b2b73d83cc
MrDavot <36482903+MrDavot@users.noreply.github.com>
2024-03-06 18:25:24 +0500

    Add files via upload

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,46 @@
+import React, {Fragment} from 'react'
+import './layout.scss'
+import {Route, Switch} from 'react-router-dom'
+
+import {AddClass} from '../../hoc/AddClass'
+import { Header } from '../header/Header';
+import { Home } from '../../pages/home/Home';
+import {Sidebar} from '../../components/sidebar/Sidebar'
+import {Calc} from '../../pages/calc/Calc'
+import {Sample} from '../../pages/sample/Sample'
+import {Info} from '../../pages/info/Info'
+
+
+ const Layout = () =>{
+
+    return(
+        <Fragment>
+        <Header/>
+
+        <div className = "content">
+            <div className = "routes">
+            <Switch>
+              <Route path = '/' exact component = {Home}/>  
+              <Route path = '/calc' render = {()=> <Calc/>}/>  
+              <Route path = '/sample' render = {()=> <Sample/>}/>  
+              <Route path = '/info' render = {()=> <Info/>}/>  
+            </Switch>
+            
+                
+            
+            
+            </div>
+
+            <Sidebar/>
+
+
+
+
+        </div>
+        </Fragment>
+    )
+}
+
+
+
+export default AddClass(Layout, 'layout')
